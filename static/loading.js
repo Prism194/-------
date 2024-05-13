be4aa3aacfddfe037faabfb4a3415e67d1641540
Na Seongchan <129660426+Prism194@users.noreply.github.com>
2024-05-13 07:35:55 +0000

    made loading function

---
+++ static/loading.js
@@ -0,0 +1,13 @@
+document.getElementById('register-form').addEventListener('submit', function(event) {
+    // Show the loading indicator
+    document.getElementById('loading-indicator').style.display = 'block';
+    
+
+  });
+
+  document.getElementById('buy-now-form').addEventListener('submit', function(event) {
+    // Show the loading indicator
+    document.getElementById('loading-indicator').style.display = 'block';
+    
+
+  });
